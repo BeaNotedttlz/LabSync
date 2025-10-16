032e2184c45d22b97cfc22054b8860feb1b38d9c
Merlin <merlin.schm@web.de>
2025-10-16 11:17:17 +0200

    Stage, Laser working TGA not sending lockmode wrong channel for normal Tab

--- LabSync.py
+++ LabSync.py
@@ -1,22 +1,20 @@
-import os
-
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QIcon
 from PySide6.QtCore import QTimer
-import sys
+import sys, os
 from LabSyncApp import MainWindow
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
 	app.setWindowIcon(QIcon(os.path.join("img", "hqe_logo.png")))
-	simulate_devices = True
+	simulate_devices = False
 
 	window = MainWindow(app, simulate_devices)
 
 	# Timer for continous calling of functions #
-	# timer = QTimer()
-	# timer.timeout.connect(window.loop_calls)
-	# timer.start(2000)
+	timer = QTimer()
+	timer.timeout.connect(window.loop_calls)
+	timer.start(2000)
 
 	# show Main window and execute loop #
 	window.show()
